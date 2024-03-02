a86e8fb9b96f240afcb1d60ea9b99a001a2a4b1c
aldolopezb <161412209+aldolopezb@users.noreply.github.com>
2024-03-02 17:26:36 -0600

    Add files via upload

---
+++ 004_Strings_Concatenar.py
@@ -0,0 +1,17 @@
+#Aldo López Barrios
+#21310106   6E
+
+
+#Con el simbolo de '+' podemos concatear también palabras o frases
+Palabra1 = "Así podemos"
+Palabra2 = "concatenar frases"
+
+#En una nueva variable concatenamos las frases para formar una sola
+Frase = Palabra1 + ' ' + Palabra2
+
+#Imprimimos la frase resultante
+print(Frase)
+
+#También podemos con la función 'print()' imprimir sin necesidad de guardar en una variable
+print("Solamente escribimos la frase o palabra que queremos imprimir")
+
