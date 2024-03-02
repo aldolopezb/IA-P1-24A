6e6de512391335f9063bc72771041e7bcd3933ab
aldolopezb <161412209+aldolopezb@users.noreply.github.com>
2024-03-02 17:42:37 -0600

    Add files via upload

---
+++ 005_title-upper-lower.py
@@ -0,0 +1,18 @@
+#Aldo López Barrios
+#21310106   6E
+
+
+'''El método title nos ayuda a poner las letras iniciales de cada palabra en mayusculas, 
+mientras que el texto faltante lo deja en minúscuals'''
+
+metodo_title = 'del teXto eN mAyusCulas Y miNuSculAs\n'
+print (metodo_title.title())
+
+
+#De la misma manera el método upper nos ayuda a pasar el texto a mayúsculas
+metodo_upper = 'del texto en que todo está en minúsculas\n'
+print(metodo_upper.upper())
+
+#Y por último el método lower que nos ayuda a tener todo en minúsculas
+metodo_lower = 'DEL TEXTO EN MAYUSCULAS'
+print(metodo_lower.lower())
