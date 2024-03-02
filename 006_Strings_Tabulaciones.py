5bd8abb6e4970ff99b65f0436de7e7343e552c7c
aldolopezb <161412209+aldolopezb@users.noreply.github.com>
2024-03-02 17:56:38 -0600

    Add files via upload

---
+++ 006_Strings_Tabulaciones.py
@@ -0,0 +1,16 @@
+#Aldo López Barrios
+#21310106   6E
+
+
+#Saltos de linea y tabulaciones en strings
+'''Esta sería una de las maneras que se pueden utilizar para los saltos de linea'''
+print('Frutas')
+print('manzana')
+print('platano')
+print('naranja')
+
+'''Pero también podemos usar '\n' '''
+print('\nFrutas\nmanzana\nplatano\nnaranja\n')
+
+#Y para la tabulación es \t
+print("Frutas\n\tmanzana\n\tplatano\n\tnaranja\n")
