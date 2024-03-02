13ef52df63c7edf832d2bae1f395c3ecf55cad6a
aldolopezb <161412209+aldolopezb@users.noreply.github.com>
2024-03-02 17:03:48 -0600

    Add files via upload

---
+++ 002_Variables.py
@@ -0,0 +1,19 @@
+#Declarar variable(s)
+texto = "Hola mundo"
+
+#Se imprime el contenido de la variable
+print(texto)
+
+'''No es necesario inicializarlas como variable de tipo entero, string, float, etc. Depende del valor que se le de cambia automáticamente'''
+
+#Se da otro valor de distinto tipo a la variable (numérico)
+texto = 254
+
+#Se imprime el valor numérico ya que cambiamos el tipo de variable al ingresarle un nuevo valor
+print(texto)
+
+cantidad = 46
+
+#Al ser las variables 'texto' y 'cantidad' variables tipo entero o valor numérico, las podríamos sumar
+
+print(texto + cantidad)
