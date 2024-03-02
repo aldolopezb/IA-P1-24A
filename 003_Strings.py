489b9a709cc175bafbb83be9ae2e917b828c3713
aldolopezb <161412209+aldolopezb@users.noreply.github.com>
2024-03-02 17:15:47 -0600

    Add files via upload

---
+++ 003_Strings.py
@@ -0,0 +1,10 @@
+#Aldo López Barrios
+#21310106   6E
+
+
+#Se declara la variable (de tipo string)
+texto = "Se pueden declarar con comillas dobles"
+texto2 = 'Y también con comillas simples'
+
+print(texto)
+print(texto2)
