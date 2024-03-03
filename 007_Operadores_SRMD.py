5810aed01fa22c9dfae59da721f967076cd2e2c7
aldolopezb <161412209+aldolopezb@users.noreply.github.com>
2024-03-02 18:04:59 -0600

    Add files via upload

---
+++ 007_Operadores_SRMD.py
@@ -0,0 +1,15 @@
+#Aldo López Barrios
+#21310106   6E
+
+
+#Los simbolos para las operaciones son (+ sumar), (- restar), (* multiplicar) y (/ dividir)
+
+suma = 4+5
+resta = 25-12
+multiplicacion = 4*11
+division = 16/4
+
+print("Resultao de la suma es:", suma)
+print("Resultao de la resta es:", resta)
+print("Resultao de la multiplicacion es:", multiplicacion)
+print("Resultao de la division es:", division)
