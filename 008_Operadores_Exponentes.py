6d782a1fd5c79e593e9b7f85e9e893e284ae6882
aldolopezb <161412209+aldolopezb@users.noreply.github.com>
2024-03-02 18:08:55 -0600

    Add files via upload

---
+++ 008_Operadores_Exponentes.py
@@ -0,0 +1,9 @@
+#Aldo López Barrios
+#21310106   6E
+
+
+'''De la misma manera que utilizabamos el signo * para multiplicar,
+ahora lo usaremos para los exponentes, pero en este caso se usan 2 (**)'''
+
+resultado = 3**4
+print(resultado)
