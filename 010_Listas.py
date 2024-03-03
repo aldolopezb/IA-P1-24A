708fd0c833fadf0233fa27df1043bd22870c9cfb
aldolopezb <161412209+aldolopezb@users.noreply.github.com>
2024-03-02 20:10:44 -0600

    Add files via upload

---
+++ 010_Listas.py
@@ -0,0 +1,9 @@
+#Aldo López Barrios
+#21310106   6E
+
+
+'''Las listas se guardan entre [] y se mandan a llamer dependiendo
+el número del lugar iniciando por el número 0'''
+
+Figuras = ['Cuadrado', 'Triangulo', 'Circulo', 'Rombo']
+print(Figuras[3])               #Se manda a llamar al rombo ya que es el número 3 de la lista
