f1b60962f0c04b7ccf1d5119267e7830378955c1
aldolopezb <161412209+aldolopezb@users.noreply.github.com>
2024-03-02 20:16:49 -0600

    Add files via upload

---
+++ 011_Listas_PosNeg.py
@@ -0,0 +1,11 @@
+#Aldo López Barrios
+#21310106   6E
+
+
+'''En las listas también se pueden acceder o mandar a llamar
+a los elementos de forma negativa'''
+
+Lista = ['Atletismo', 'Natación', 'Futbol', 'Gimnasia', 'Tenis']
+
+#Si mandamos a llamar al elemento -2 llamaría al elemento 'Gimnasia'
+print(Lista[-2])
