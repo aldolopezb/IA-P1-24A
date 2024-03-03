caaae6e7d75bf033efc30abc7d72ff75f416ba4b
aldolopezb <161412209+aldolopezb@users.noreply.github.com>
2024-03-02 18:15:30 -0600

    Add files via upload

---
+++ 009_FloatYRound.py
@@ -0,0 +1,15 @@
+#Aldo López Barrios
+#21310106   6E
+
+
+'''Al tener número con desimales (floats) y queremos redondearlos
+a cierto numero de decimales usamos 'round' '''
+
+#Hacemos una división que nos de varios decimales
+operacion = 10/3
+print(operacion)
+
+#Pero si nosotros solo queremos tener 3 decimales usamos 'round'
+
+#El primer dato es el que queremos redondear y el segundo el numero de decimales que queremos
+print(round(operacion,3)) 
